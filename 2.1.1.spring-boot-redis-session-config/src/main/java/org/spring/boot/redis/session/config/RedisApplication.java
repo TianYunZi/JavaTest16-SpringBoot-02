c79329f2cbe1f29c523e76d83949e44b1e9bb067
jinxin.xie <jinxin.xie@siebre.com>
2018-03-19 18:16:51 +0800

    redis config 配置

---
+++ 2.1.1.spring-boot-redis-session-config/src/main/java/org/spring/boot/redis/session/config/RedisApplication.java
@@ -0,0 +1,12 @@
+package org.spring.boot.redis.session.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+}
---
+++ 2.1.1.spring-boot-redis-session-config/src/main/java/org/spring/boot/redis/session/config/config/RedisConfig.java
@@ -0,0 +1,11 @@
+package org.spring.boot.redis.session.config.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+}
